0dd27e3a976dcc1e652352a0c3592d44c89f2732
Yanislav Igonin <jan.oasis@gmail.com>
2019-01-03 21:11:27 +0300

    Add pino-pretty as dev dep

--- src/modules/logger.mjs
+++ src/modules/logger.mjs
@@ -3,7 +3,7 @@ import pino from 'pino';
 const env = process.env.NODE_ENV;
 
 const logger = {
-  development: pino({ timestamp: false, level: 'debug' }),
+  development: pino({ timestamp: false, level: 'debug', prettyPrint: true }),
 };
 
 export default logger[env];
