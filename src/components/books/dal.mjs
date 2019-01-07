ad4ed20a0f91c6002ced9f484a5f230f90c42741
Yanislav Igonin <jan.oasis@gmail.com>
2019-01-08 01:59:49 +0300

    Add sorting functional in list method

--- src/components/books/dal.mjs
+++ src/components/books/dal.mjs
@@ -11,9 +11,27 @@ const dal = {
         b.image, 
         a.first_name AS author_first_name, 
         a.last_name AS author_last_name 
-      FROM books AS b 
-      JOIN authors AS a ON b.author=a.id`;
+      FROM books AS b
+      JOIN authors AS a ON b.author=a.id
+    `;
     const values = [];
+
+    // May cause SqlI attacks, but inserting sort criteria in values
+    // array will result in string escaping (e.g. ORDER BY 'id' 'asc').
+    // This method isn't working, anyway, for me and mysql5.
+    // Couldn't find better method for now.
+    if (query.sort) {
+      sql += ' ORDER BY ';
+      query.sort.forEach((sortCriteria) => {
+        sortCriteria.forEach((value) => {
+          sql += ` ${value}`;
+        });
+
+        sql += ',';
+      });
+
+      sql = sql.slice(0, -1);
+    }
 
     if (query.limit) {
       sql += ' LIMIT ?';
