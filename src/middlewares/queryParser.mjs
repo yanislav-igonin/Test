cbf94738a9e0e4fe3fb39774c4fa83822355b5db
Yanislav Igonin <jan.oasis@gmail.com>
2019-01-08 00:23:13 +0300

    Update query parser middleware

--- src/middlewares/queryParser.mjs
+++ src/middlewares/queryParser.mjs
@@ -1,24 +1,6 @@
-const parseSort = (sortString) => {
-  const [key, value] = sortString.split(':');
-
-  const sortObj = {};
-
-  sortObj[key] = value;
-
-  return sortObj;
-};
+const parseSort = sortString => sortString.split(':');
 
 export default async (ctx, next) => {
-  const parsedQuery = {};
-
-  if (ctx.query.limit) {
-    parsedQuery.limit = ctx.query.limit;
-  }
-
-  if (ctx.query.offset) {
-    parsedQuery.offset = ctx.query.offset;
-  }
-
   if (ctx.query.sort) {
     if (Array.isArray(ctx.query.sort)) {
       ctx.query.sort = ctx.query.sort.map(value => parseSort(value));
@@ -26,8 +8,6 @@ export default async (ctx, next) => {
       ctx.query.sort = [parseSort(ctx.query.sort)];
     }
   }
-
-  ctx.query = parsedQuery;
 
   await next();
 };
